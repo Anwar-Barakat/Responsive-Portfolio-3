d6d9e7defb2ab810997305fe87b6c5ccb3785abe
Anwar Barakat <brkatanwar0@gmail.com>
2022-05-16 12:56:22 +0300

    working on skills section part(3). ending it

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,22 @@ skills.forEach(skill => {
     skillName.style.width = skillValue;
 });
 //? =========================== Fill skills bar dynamically =================
+
+
+//? =========================== According Skills ============================
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+    for (i = 0; i < skillsContent.length; i++)
+        skillsContent[i].className = 'skills__content skills__close';
+
+    if (itemClass === 'skills__content skills__close')
+        this.parentNode.className = 'skills__content skills__open'
+}
+
+skillsHeader.forEach((header) => {
+    header.addEventListener('click', toggleSkills)
+});
+//? =========================== According Skills ============================
